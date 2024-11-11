f85c3077f794476a16042956bb501558bf405238
carlosthodoro <164918114+carlosthodoro@users.noreply.github.com>
2024-11-11 14:09:40 -0300

    style

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+crriaCartao{
+    "Aleatório",
+    "Qual o motivo da morte de Hitler?",
+    "Suicidio",
+}
+
+crriaCartao{
+    "Geografia",
+    "Qual é a capital da França?",
+    "Suicidio",
+}
+
+crriaCartao{
+    "Aleatório",
+    "Qual o motivo da morte de Hitler?",
+    "Paris",
+}
+
+crriaCartao{
+    "Aleatório",
+    "Qual o motivo da morte de Hitler?",
+    "Suicidio",
+}
+
+crriaCartao{
+    "Aleatório",
+    "Quem matou Getulio Vargas?",
+    "Ele mesmo",
+}
+
+crriaCartao{
+    "Aleatório",
+    "Quem perdeu duas guerras mundiais?",
+    "Alemanha",
+}
