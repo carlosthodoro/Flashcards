c9a94c71232dafa00a27efc18a2dd216b216e9e7
carlosthodoro <164918114+carlosthodoro@users.noreply.github.com>
2024-11-18 13:48:05 -0300

    app

--- app.js
+++ app.js
@@ -2,4 +2,17 @@
         let container = document.getElementById('container')
         let cartao = document.createElement('article')
         cartao.className = 'cartao'
+
+        cartao.innerHTML = `
+        <div class="cartao__conteudo">
+                    <h3>Aleatório</h3>
+                    <div class="cartao__conteudo__pergunta">
+                    <p>Qual o motivo da morte de Hitler?</p>
+                    </div>
+                    <div class="cartao_-conteudo__resposta">
+                    <p>Suicidio.</p>
+                    </div>
+                </div>
+                `
+                container.appendChild(cartao)
     }
